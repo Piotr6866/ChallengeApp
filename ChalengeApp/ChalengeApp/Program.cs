4034bc08bf1e2b2b02054f87fcfda38ae11c29db
PiotrK <Piotr_Kotecki@hotmail.com>
2023-02-05 23:32:40 +0100

    Zadanie Domowe: Dzień czwarty

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Piotr! Co słychać");
+//Console.WriteLine("Hello, Piotr! Co słychać");
+var name = "Ewa";
+var sex = "Kobieta";
+var age = 33;
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "Mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Nieznany warunek");
+}
+
+
