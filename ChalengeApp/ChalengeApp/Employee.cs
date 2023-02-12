ccadcd95d294cfa7d220471d6dc561bf6bff2c58
PiotrK <Piotr_Kotecki@hotmail.com>
2023-02-12 15:58:49 +0100

    Dzien 6 Zadanie domowe

---
+++ ChalengeApp/ChalengeApp/Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace ChalengeApp
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        private List<int> Score = new List<int>();
+        public Employee(string Name, string lastName, int age)
+        {
+            this.Name = Name;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+        public void AddScore(int number)
+        {
+            this.Score.Add(number);
+        }
+        public int Result
+        {
+            get 
+            { 
+                return this.Score.Sum(); 
+            }
+        }
+    }
+}
--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,17 +1,43 @@
-﻿long number = 1111110234566;  // typ long ze względu na moźliwość użycia dłuższych liczb
-//long number2 = long.MaxValue;
-string numberAsString = number.ToString();
-char[] Digits = numberAsString.ToArray();
-int[] counter = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+﻿using ChalengeApp;
 
-foreach (char digit in Digits)
+Employee Employee1 = new Employee("Adam", "Nowak", 35);
+Employee1.AddScore(3);
+Employee1.AddScore(5);
+Employee1.AddScore(7);
+Employee1.AddScore(9);
+Employee1.AddScore(10);
+
+Employee Employee2 = new Employee("Wojciech", "Fibak", 58);
+Employee2.AddScore(2);
+Employee2.AddScore(7);
+Employee2.AddScore(7);
+Employee2.AddScore(8);
+Employee2.AddScore(8);
+
+Employee Employee3 = new Employee("Luke", "Skywalker", 20);
+Employee3.AddScore(4);
+Employee3.AddScore(5);
+Employee3.AddScore(7);
+Employee3.AddScore(10);
+Employee3.AddScore(10);
+
+List<Employee> Employees = new List<Employee>()
 {
-    counter[Convert.ToInt32(new string(digit, 1))]++;
+    Employee1,Employee2,Employee3
+};
+
+int maxResult = -1;
+Employee EmployeeWithMaxResult = null;
+
+foreach (var Employee in Employees)
+{
+    if (Employee.Result > maxResult)
+    {
+        EmployeeWithMaxResult = Employee;
+        maxResult = Employee.Result;
+    }
 }
 
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine(i + " => " + counter[i]);
-}
-
-
+Console.WriteLine("Pracownik z najwyższym wynikiem: " + EmployeeWithMaxResult.Name + 
+    " " + EmployeeWithMaxResult.LastName + ", lat: " + EmployeeWithMaxResult.Age);
+Console.WriteLine("Wynik: " + EmployeeWithMaxResult.Result);
